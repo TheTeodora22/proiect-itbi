445abf1bd32451a0b480c1fb76f5e38a6595f59c
Teo <simionov.teodora22@gmail.com>
2025-01-09 18:04:35 +0200

    tried special char case

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+text="Empire Burlesque &lt;"
+text=$(echo "$text" | sed -e 's/&amp;/\&/g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&quot;/"/g' -e "s/&apos;/'/g")
+echo "$text"
+text=$(echo "$text" |  sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g' -e 's/"/\&quot;/g' -e "s/'/\&apos;/g")
+echo "$text"
